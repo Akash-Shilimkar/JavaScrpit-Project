8103934c794bfcf7a68df7d36edec30679b37333
Akash-Shilimkar <akashshilimkar904@gmail.com>
2024-02-14 23:04:32 +0530

    write  Script and do some other changes .

--- Script.js
+++ Script.js
@@ -1,10 +1,17 @@
 async function fetchWeatherData(cityName){
 	let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=24c1524d8b37eda85bebe85c3b5b8459`;
-	const response = await fetch(url)
+	const response = await fetch(url);
 	const data = await response.json();
-	console.log(data)
+	let city = document.getElementById("city");
+	city.innerHTML = data.name;
+	let windSpeed = document.getElementById("wind_speed");
+	windSpeed.innerHTML =data.wind.speed+" km/hr"; 
+	let humidity = document.getElementById("humidity");
+	humidity.innerHTML = data.main.humidity+" g/kg";
+	let temp = document.getElementById("temp");
+	temp.innerHTML = Math.round((data.main.temp)-273.15)+"°C";
+	console.log(data);
 }
-
 
 function fetchCity(){
 	let cityName = document.getElementById("city_name");
